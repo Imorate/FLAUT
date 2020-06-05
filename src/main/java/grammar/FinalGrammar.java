f6e03954c5802b6807e26e0d665d866fc84184a2
Amir Mohammad Hl <dev.imorate@gmail.com>
2020-06-06 00:27:11 +0430

    Formatted grammar added

--- src/main/java/grammar/FinalGrammar.java
+++ src/main/java/grammar/FinalGrammar.java
@@ -4,6 +4,9 @@ import grammar.interfaces.GrammarParser;
 import grammar.model.Grammar;
 import lombok.Data;
 
+import java.util.Map;
+import java.util.Set;
+
 @Data
 public class FinalGrammar {
     private Grammar grammar;
@@ -23,5 +26,17 @@ public class FinalGrammar {
 
     public Grammar removeLambdaGrammar() {
         return new RemoveLambdaImpl(leftRecursionGrammar()).removeLambda();
+    }
+
+    public String formattedGrammar() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry<String, Set<String>> entry : this.grammar.getProductions().entrySet()) {
+            stringBuilder.append(entry.getKey()).append("\t->\t");
+            entry.getValue().forEach(s -> stringBuilder.append(s).append("|"));
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            stringBuilder.append("\n");
+        }
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        return stringBuilder.toString();
     }
 }
