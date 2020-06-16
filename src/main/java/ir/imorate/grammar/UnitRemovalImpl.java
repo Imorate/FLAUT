b7c9ec47516cf4ea80fc0fcc5b8b08bfefc2aae6
Amir Mohammad Hl <dev.imorate@gmail.com>
2020-06-16 15:49:05 +0430

    Fix UnitRemoval major bugs for cover all possible grammars

--- src/main/java/ir/imorate/grammar/UnitRemovalImpl.java
+++ src/main/java/ir/imorate/grammar/UnitRemovalImpl.java
@@ -1,6 +1,5 @@
 package ir.imorate.grammar;
 
-import com.rits.cloning.Cloner;
 import ir.imorate.grammar.interfaces.UnitRemoval;
 import ir.imorate.grammar.model.Grammar;
 import ir.imorate.graph.Graph;
@@ -21,42 +20,26 @@ public class UnitRemovalImpl implements UnitRemoval {
         this.dependencyGraph = mapToDependencyGraph();
     }
 
-    public static Set<String> mergeSet(Set<String> a, Set<String> b) {
-        return new HashSet<>() {
-            {
-                addAll(a);
-                addAll(b);
-            }
-        };
-    }
-
     @Override
     public Grammar unitRemovalFix() {
-        if (hasLoopyNode()) {
-            Grammar nonUnitGrammar = filterNonUnitProduction();
-            Grammar newGrammar = generateNewGrammar();
-            for (Map.Entry<String, Set<String>> entry : newGrammar.getProductions().entrySet()) {
-                entry.setValue(mergeSet(entry.getValue(), nonUnitGrammar.getProductions().get(entry.getKey())));
+        System.out.println(this.dependencyGraph);
+        for (Map.Entry<Node<String>, LinkedList<Node<String>>> entry : this.dependencyGraph.getAdjacencyMap().entrySet()) {
+            if (!entry.getValue().isEmpty()) {
+                Node<String> nodeVar = entry.getKey();
+                for (Node<String> node : entry.getValue()) {
+                    if (ProductionType.isUnitProduction(node.getLabel())) {
+                        this.grammar = updateGrammar(nodeVar.getLabel(), node.getLabel(), bfsTraverse(node.getLabel()));
+                    }
+                }
             }
-            this.grammar = newGrammar;
-        } else {
-            this.grammar = fixWithoutLoopyNode();
         }
         return this.grammar;
     }
 
-    public Grammar generateNewGrammar() {
-        Grammar newGrammar = new Grammar();
-        newGrammar.setStartVariable(this.grammar.getStartVariable());
-        for (Map.Entry<String, Set<String>> entry : this.grammar.getProductions().entrySet()) {
-            for (String rule : entry.getValue()) {
-                if (ProductionType.isUnitProduction(rule)) {
-                    String key = entry.getKey();
-                    newGrammar.getProductions().put(key, bfsTraverse(key));
-                }
-            }
-        }
-        return newGrammar;
+    public Grammar updateGrammar(String var, String rule, Set<String> ruleSet) {
+        this.grammar.getProductions().get(var).remove(rule);
+        this.grammar.getProductions().get(var).addAll(ruleSet);
+        return this.grammar;
     }
 
     public Set<String> bfsTraverse(String source) {
@@ -68,9 +51,7 @@ public class UnitRemovalImpl implements UnitRemoval {
         while (!queue.isEmpty()) {
             Node<String> visitedNode = queue.remove();
             visitedNodes.add(visitedNode);
-            if (!visitedNode.equals(sourceNode)) {
-                aggregateRules.addAll(getTerminalProduction(visitedNode));
-            }
+            aggregateRules.addAll(getNonUnitProduction(visitedNode));
             List<Node<String>> neighbours = this.dependencyGraph.getAdjacencyMap().get(visitedNode);
             if (neighbours != null) {
                 for (Node<String> neighbourNode : neighbours) {
@@ -83,11 +64,11 @@ public class UnitRemovalImpl implements UnitRemoval {
         return aggregateRules;
     }
 
-    public Set<String> getTerminalProduction(Node<String> node) {
+    public Set<String> getNonUnitProduction(Node<String> node) {
         return this.grammar.getProductions().entrySet().stream()
                 .filter(stringSetEntry -> stringSetEntry.getKey().equals(node.getLabel()))
                 .flatMap(stringSetEntry -> stringSetEntry.getValue().stream())
-                .filter(ProductionType::isTerminalProduction)
+                .filter(ProductionType::isNonUnitProduction)
                 .collect(Collectors.toSet());
     }
 
@@ -101,40 +82,5 @@ public class UnitRemovalImpl implements UnitRemoval {
             }
         }
         return graph;
-    }
-
-    public Grammar filterNonUnitProduction() {
-        Grammar nonUnitGrammar = new Cloner().deepClone(this.grammar);
-        nonUnitGrammar.getProductions().entrySet()
-                .forEach(entry -> entry.setValue(entry.getValue().stream()
-                        .filter(ProductionType::isNonUnitProduction).collect(Collectors.toSet())));
-        return nonUnitGrammar;
-    }
-
-    private boolean hasLoopyNode() {
-        for (Map.Entry<Node<String>, LinkedList<Node<String>>> entry : dependencyGraph.getAdjacencyMap().entrySet()) {
-            if (!entry.getValue().isEmpty()) {
-                for (Node<String> node : entry.getValue()) {
-                    if (dependencyGraph.hasBidirectionalTraverse(entry.getKey().getLabel(), node.getLabel())) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    private Grammar fixWithoutLoopyNode() {
-        Grammar fixedGrammar = this.grammar;
-        for (Map.Entry<Node<String>, LinkedList<Node<String>>> entry : dependencyGraph.getAdjacencyMap().entrySet()) {
-            if (!entry.getValue().isEmpty()) {
-                for (Node<String> node : entry.getValue()) {
-                    Set<String> productionSet = fixedGrammar.getProductions().get(entry.getKey().getLabel());
-                    productionSet.remove(node.getLabel());
-                    productionSet.addAll(fixedGrammar.getProductions().get(node.getLabel()));
-                }
-            }
-        }
-        return fixedGrammar;
     }
 }
